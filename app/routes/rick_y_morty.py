1b1c6e923b25e87ffdc0d475feef498ccf0a17dc
Degoxec98 <degoxec98@gmail.com>
2022-11-25 18:41:27 -0500

    routes: insertar_personajes() added

--- app/routes/rick_y_morty.py
+++ app/routes/rick_y_morty.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, request, flash
-from app.models.rick_y_morty import Personajes
+from app.models.personaje import Personaje
 from app.db import db
 from app.requests import Request
 
@@ -7,18 +7,20 @@ from app.requests import Request
 rick_y_morty = Blueprint('rick_y_morty', __name__)
 
 
-@rick_y_morty.route('/')
+@rick_y_morty.route('/', methods=['GET'])
 def index():
-    personajes=Personajes()
+    personajes=Personaje()
 
     return render_template('index.html',personajes=personajes)
 
 
-@rick_y_morty.route('/insertar-personaje')
+@rick_y_morty.route('/insertar-personaje', methods = ['GET', 'POST'])
 def insertar_personaje():
-    personajes = Request()
-    db.personajes.insert_many(personajes.characters())
+    personajes = Request().characters()
+    print("Personaje 1: ", personajes[0])
+    db.personajes.insert_many(personajes)
     # db.personajes.insert_one(personajes[0])
+    return 'PERSONAJES INSERTADOS!'
     
 
 
