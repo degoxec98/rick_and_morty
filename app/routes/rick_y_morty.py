ad3a607032e5b973aecf743d9d89b60b55a85a38
Degoxec98 <degoxec98@gmail.com>
2022-11-25 22:20:34 -0500

    profile() added

--- app/routes/rick_y_morty.py
+++ app/routes/rick_y_morty.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request, flash
+from flask import Blueprint, render_template, redirect, request, flash, url_for
 from app.models.personaje import Personaje
 from app.db import db
 from app.requests import Request
@@ -9,22 +9,26 @@ rick_y_morty = Blueprint('rick_y_morty', __name__)
 
 @rick_y_morty.route('/', methods=['GET'])
 def index():
-    personajes=Personaje()
-
+    personajes = db.personajes.find()
     return render_template('index.html',personajes=personajes)
 
 
 @rick_y_morty.route('/insertar-personaje', methods = ['GET', 'POST'])
 def insertar_personaje():
     personajes = Request().characters()
-    print("Personaje 1: ", personajes[0])
     db.personajes.insert_many(personajes)
-    # db.personajes.insert_one(personajes[0])
     return 'PERSONAJES INSERTADOS!'
-    
 
 
-@rick_y_morty.route('/perfil')
-def profile():
+@rick_y_morty.route('/profile', defaults = {'id': None})
+@rick_y_morty.route('/profile/', defaults = {'id': None})
+@rick_y_morty.route('/profile/<int:id>')
+def profile(id = None):
+    if id == None:
+        return redirect('/')
+    personaje = db.personajes.find_one({'id':int(id)})
+    if personaje == None:
+        return redirect('/')
     
-    pass
+    return render_template("profile.html", personaje = personaje)
+    
